52145d6cbba4633b8250d12d5aa0a496789d8f32
Shob-hit-10 <shobhitkoli4600@gmail.com>
2025-04-02 07:36:07 +0000

    02

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//Treat all JS code as newer version
+//alert(3+3) We are using nodeJs not browser
+
+let name = "shobhit"
+let age = 23
+let isLoggedIn = false
+let state;
+
+//Number = 2 to power 53
+//bigint
+//string = ""
+//boolean = true or false
+//null = standalone value
+//undefined
+//symbol = unique
+
+//object
+console.log(typeof null)
+
+//typeof NULL is object
+//type of undefined is undefined
