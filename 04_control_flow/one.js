1845486c115774bff0875bfd61cceabc205e4b53
Shob-hit-10 <shobhitkoli4600@gmail.com>
2025-04-09 10:37:22 +0000

    13

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+//if
+
+const isUserLoggedIn = true;
+if(2=="2"){
+    console.log("ok");
+}
+if(2==="2"){
+    console.log('notOk')
+}
+else{
+    console.log("It will print.");
+}
+
+const score=200;
+if(score>100){
+    const power="fly";
+    console.log(`User power: ${power}`);
+}
+
+const balance = 1000;
+if(balance > 500) console.log("test");
+
+if(balance > 500){
+    console.log("YES");
+}
+else if(balance < 900){
+    console.log("YES YES");
+}
+else{
+    console.log("YES YES YES");
+}
